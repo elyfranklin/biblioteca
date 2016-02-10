9b3178ab35e2eba10d15f8f95e21861009402adf
Ely <Ely@pc-ely>
2016-02-10 15:41:58 -0300

    Alterações em Janela

--- src/biblioteca/Janela.java
+++ src/biblioteca/Janela.java
@@ -1,9 +1,18 @@
 package biblioteca;
 
-public class Janela {
+import java.awt.Window;
+
+import javax.swing.JFrame;
+
+public class Janela extends JFrame{
 
 	public Janela() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub/
+		super("Biblioteca");
+		setLocationRelativeTo(null);//centralizar
+		setSize(Window.WIDTH,Window.HEIGHT);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 
 }
